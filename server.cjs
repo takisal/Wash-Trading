6f543b13ad57f25385d19358a67e8a07ea5e4e5b
Eric <ericsanchirico@yahoo.com>
2023-12-02 17:02:53 -0500

    secondary commit

---
+++ server.cjs
@@ -0,0 +1,99 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+//var cert = fs.readFileSync('/etc/letsencrypt/live/morphswap.io/fullchain.pem');
+//var key = fs.readFileSync('/etc/letsencrypt/live/morphswap.io/privkey.pem');
+//var options = { key: key, cert: cert };
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
+let PORT = 3000;
+app.get("/getMinAmount", async function (req, res) {
+  let minAmount = await getMinAmountFromBTCtoXMR();
+  res.send(minAmount);
+});
+app.post("/estimate", async function (req, res) {
+  let estimated = await estimateRecievedXMR(req.body.amount);
+  res.send(estimated);
+});
+const api_key = "0e5b7c3b8d1144040a6ec82b334d74b1c686e8ce9728a151b83d204b91d997fa";
+async function getMinAmountFromBTCtoXMR() {
+  const response = await fetch("https://api.changenow.io/v1/min-amount/btc_xmr?api_key=" + api_key);
+  const minAmount = await response.json();
+  return minAmount;
+}
+async function estimateRecievedXMR(amount) {
+  let reqStr = "https://api.changenow.io/v1/exchange-amount/" + amount + "/btc_xmr?api_key=" + api_key;
+
+  let response = await fetch(reqStr);
+  let jsonResponse = await response.json();
+  return jsonResponse;
+}
+//https://api.changenow.io/v1/transactions/:id/:api_key
+
+function trim(num, maxDec) {
+  let d = false;
+  let c = 0;
+  for (let i = 0; i < num.length && c < maxDec; i++) {
+    if (num[i] == ".") {
+      d = true;
+    } else {
+      if (d) {
+        c++;
+      }
+    }
+  }
+}
+async function getTransactionStatus(id) {
+  const response = await fetch("https://api.changenow.io/v1/transactions/" + id + "/" + api_key);
+  const statusR = await response.json();
+  return statusR;
+}
+async function createBTCtoXMRtransaction(url = "", amount, xmrAddress) {
+  // Default options are marked with *
+  let data = {
+    from: "btc",
+    to: "xmr",
+    address: xmrAddress,
+    amount: trim(amount.toString(), 6),
+    extraId: "",
+    userId: "",
+    contactEmail: "",
+    refundAddress: "",
+    refundExtraId: "",
+  };
+
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+  /*Example Response:
+  {
+  "payinAddress": "328E95juhLbXeDDVDR9thh58MtCsnKuvf6",
+  "payoutAddress": "0x57f31ad4b64095347F87eDB1675566DAfF5EC886",
+  "payoutExtraId": "",
+  "fromCurrency": "btc",
+  "toCurrency": "eth",
+  "refundAddress": "",
+  "refundExtraId": "",
+  "id": "33d9b8e1867579",
+  "amount": 74.7999317
+}*/
+}
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,20 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+/*
+API - Get the minimum exchange amount for the selected currency pair with the
+
+'Minimal Exchange Amount'
+
+method;
+
+UI - Ask a user for the amount to exchange and check if this amount is bigger than the minimum exchange amount;
+
+API - Call the
+
+'Estimated Exchange Amount'
+
+method to get the estimated amount for the exchange (in our example, ETH estimated amount);
+
+UI - Show a user the estimated exchange amount and ask for confirmation;
+
+UI - Ask a user for their wallet address to send the funds after the exchange is completed (their refund address, extra ID, refund extra ID);
+
+API - Call the
+
+'Create Exchange Transaction'
+
+method to create an exchange and get the deposit address (in our example, the generated BTC wallet address is returned from this method);
+
+UI - Ask a user to send the funds that they want to exchange to the generated deposit address (in our example, user has to send BTC coins);
+
+UI - A user sends coins, ChangeNOW performs an exchange and makes a withdrawal to user address (in our example, to their ETH address);
+
+API - With
+
+'Transaction status'
+
+you can get the transaction status and display it to a user for them to obtain all the info on the exchange.
+
+*/
+
+function getMinAmount() {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:3000/getMinAmount", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => console.log("error", error));
+}
+function estimateReceived(amount) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  let raw = JSON.stringify({ amount });
+  console.log(raw);
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    redirect: "follow",
+    body: raw,
+  };
+  console.log(requestOptions);
+  fetch("http://localhost:3000/estimate", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      let lowEndTime = "";
+      let highendTime = "";
+      let preDash = true;
+      for (let i = 0; i < result.transactionSpeedForecast.length; i++) {
+        if (result.transactionSpeedForecast[i] === "-") {
+          preDash = false;
+        } else {
+          if (preDash) {
+            lowEndTime += result.transactionSpeedForecast[i];
+          } else {
+            highendTime += result.transactionSpeedForecast[i];
+          }
+        }
+      }
+      lowEndTime = parseInt(lowEndTime);
+      highendTime = parseInt(highendTime);
+      console.log(
+        "Time Low Estimate: ",
+        lowEndTime,
+        "Time High Estimate: ",
+        highendTime,
+        "EstimatedAmount: ",
+        result.estimatedAmount
+      );
+    })
+    .catch((error) => console.log("error", error));
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <button onClick={getMinAmount}>Get Minimum Amount</button>
+        <button onClick={estimateReceived.bind(this, 13)}>Estimate Received Amount</button>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
